ce68397a7cf7ee03f1cc7ada507b3fe53f1e8918
Rishav Kumar <kumarrishav14@gmail.com>
2021-04-02 21:28:27 +0530

    added few more register

--- wdog_reg_block.sv
+++ wdog_reg_block.sv
@@ -2,25 +2,69 @@ class wdog_reg_block extends uvm_reg_block;
 
     `uvm_object_utils(wdog_reg_block)
     
-    Wdog_load wdog_load;
-    Wdog_lock wdog_lock;
+    // Register objects
+    Wdog_load       wdog_load;
+    Wdog_lock       wdog_lock;
+    Wdog_value      wdog_value;
+    Wdog_control    wdog_control;
+    Wdog_intclr     wdog_intclr;
+    Wdog_ris        wdog_ris;
+    Wdog_mis        wdog_mis;
 
     function new(string name = "reg_file");
         super.new(name);
     endfunction //new()
 
+    // Builds the register block
+    // --> build() the registers
+    // --> configure() the register
     function void build();
-        wdog_lock = Wdog_lock::type_id::create();
-        wdog_lock.build();
-        wdog_lock.configure(this);
         
+        // 1. WDOG_LOAD
         wdog_load = Wdog_load::type_id::create();
         wdog_load.build();
         wdog_load.configure(this);
 
+        // 2. WDOG_VALUE
+        wdog_value = new();
+        wdog_value.build();
+        wdog_value.configure(this);
+
+        // 3. WDOG_CONTROL
+        wdog_control = new();
+        wdog_control.build();
+        wdog_control.configure(this);
+
+        // 4. WDOG_INTCLR
+        wdog_intclr = new();
+        wdog_intclr.build();
+        wdog_intclr.configure(this);
+
+        // 5. WDOG_RIS
+        wdog_ris = new();
+        wdog_ris.build();
+        wdog_ris.configure(this);
+
+        // 6. WDOG_MIS
+        wdog_mis = new();
+        wdog_mis.build();
+        wdog_mis.configure(this);
+
+        // 7. WDOG_LOCK
+        wdog_lock = Wdog_lock::type_id::create();
+        wdog_lock.build();
+        wdog_lock.configure(this);
+
+
+        // Create address map for the registers
         default_map = create_map("wdog_reg_map", 0, 4, UVM_LITTLE_ENDIAN);
-        default_map.add_reg(wdog_lock, 'hC00, "RW");
-        default_map.add_reg(wdog_load, 'h0, "RW");
+        default_map.add_reg(wdog_load,      'h0,    "RW");
+        default_map.add_reg(wdog_value,     'h4,    "RO");
+        default_map.add_reg(wdog_control,   'h8,    "RW");
+        default_map.add_reg(wdog_intclr,    'hC,    "WO");
+        default_map.add_reg(wdog_ris,       'h10,   "RO");
+        default_map.add_reg(wdog_mis,       'h14,   "RO");
+        default_map.add_reg(wdog_lock,      'hC00,  "RW");
 
         lock_model();
     endfunction: build 
--- wdog_registers.sv
+++ wdog_registers.sv
@@ -1,18 +1,15 @@
-class Wdog_lock extends uvm_reg;
-    `uvm_object_utils(Wdog_lock)
-    
-    uvm_reg_field wdog_lock;
-    function new(string name = "wdog_lock");
-        super.new(name, 32, UVM_NO_COVERAGE);
-    endfunction //new()
-
-    function void build();
-        wdog_lock = uvm_reg_field::type_id::create("wdog_lock");
-        wdog_lock.configure(this, 32, 0, "RW", 0, 32'h1ACCE551, 1, 1, 1);
-    endfunction: build
-    
-endclass //wdog_lock extends uvm_reg
-
+//function void configure(uvm_reg 	parent,
+                // int 	unsigned 	size,
+                // int 	unsigned 	lsb_pos,
+                // string 	access,
+                // bit 	volatile,
+                // uvm_reg_data_t 	reset,
+                // bit 	has_reset,
+                // bit 	is_rand,
+                // bit 	individually_accessible	)
+// *************************************************************
+// 1. WDOG_LOAD (Load Register)
+// *************************************************************
 class Wdog_load extends uvm_reg;
     `uvm_object_utils(Wdog_load)
     
@@ -24,7 +21,124 @@ class Wdog_load extends uvm_reg;
 
     function void build();   
         wdog_load = uvm_reg_field::type_id::create("wdog_load");
-        wdog_load.configure(this, 32, 0, "RW", 0, 32'hFFFFFFFF, 1, 1, 1);
+        wdog_load.configure(this, 32, 0, "RW", 0, 32'hFFFFFFFF, 0, 1, 0);
+    endfunction: build
+endclass //Wdog_load extends uvm_reg
+
+
+// *************************************************************
+// 2. WDOG_Value (Vaue Register)
+// *************************************************************
+class Wdog_value extends uvm_reg;
+    `uvm_object_utils(Wdog_value)
+    
+    uvm_reg_field wdog_value;
+    function new(string name = "wdog_value");
+        super.new(name, 32, UVM_NO_COVERAGE);
+    endfunction //new()
+
+    function void build();
+        wdog_value = uvm_reg_field::type_id::create("wdog_value");
+        wdog_value.configure(this, 32, 0, "RO", 1, 32'h0, 0, 0, 0);
     endfunction: build
     
-endclass //Wdog_load extends uvm_reg
+endclass //wdog_lock extends uvm_reg
+
+
+// *************************************************************
+// 3. WDOG_Control (Control Register)
+//    WDOG_Control[0] = inten
+//    WDOG_Control[1] = resen
+// *************************************************************
+class Wdog_control extends uvm_reg;
+    `uvm_object_utils(Wdog_value)
+    
+    uvm_reg_field inten;
+    uvm_reg_field resen;
+
+    function new(string name = "wdog_value");
+        super.new(name, 32, UVM_NO_COVERAGE);
+    endfunction //new()
+
+    function void build();
+        inten = uvm_reg_field::type_id::create("inten");
+        inten.configure(this, 1, 0, "RW", 0, 1'h0, 1, 1, 0);
+
+        resen = uvm_reg_field::type_id::create("resen");
+        resen.configure(this, 1, 1, "RW", 0, 1'h0, 1, 1, 0);
+    endfunction: build
+    
+endclass //wdog_lock extends uvm_reg
+
+// *************************************************************
+// 4. WDOG_INTCLR (Interupt Clear Register)
+// *************************************************************
+class Wdog_intclr extends uvm_reg;
+    `uvm_object_utils(Wdog_lock)
+    
+    uvm_reg_field wdog_intclr;
+    function new(string name = "wdog_intclr");
+        super.new(name, 32, UVM_NO_COVERAGE);
+    endfunction //new()
+
+    function void build();
+        wdog_intclr = uvm_reg_field::type_id::create("wdog_intclr");
+        wdog_intclr.configure(this, 32, 0, "WO", 0, 32'h0, 0, 1, 1);
+    endfunction: build
+    
+endclass //wdog_intclr extends uvm_reg
+
+// *************************************************************
+// 5. WDOG_RIS (Raw Interrupt Register)
+// *************************************************************
+class Wdog_ris extends uvm_reg;
+    `uvm_object_utils(Wdog_lock)
+    
+    uvm_reg_field wdog_ris;
+    function new(string name = "wdog_ris");
+        super.new(name, 32, UVM_NO_COVERAGE);
+    endfunction //new()
+
+    function void build();
+        wdog_ris = uvm_reg_field::type_id::create("wdog_ris");
+        wdog_ris.configure(this, 1, 0, "RO", 0, 32'h0, 1, 1, 1);
+    endfunction: build
+    
+endclass //wdog_ris extends uvm_reg
+
+
+// *************************************************************
+// 6. WDOG_MIS(Masked Interrupt Register)
+// *************************************************************
+class Wdog_mis extends uvm_reg;
+    `uvm_object_utils(Wdog_lock)
+    
+    uvm_reg_field wdog_mis;
+    function new(string name = "wdog_mis");
+        super.new(name, 32, UVM_NO_COVERAGE);
+    endfunction //new()
+
+    function void build();
+        wdog_mis = uvm_reg_field::type_id::create("wdog_mis");
+        wdog_mis.configure(this, 32, 0, "RO", 0, 32'h0, 1, 1, 1);
+    endfunction: build
+    
+endclass //wdog_mis extends uvm_reg
+
+// *************************************************************
+// 7. WDOG_LOCK (Lock Register)
+// *************************************************************
+class Wdog_lock extends uvm_reg;
+    `uvm_object_utils(Wdog_lock)
+    
+    uvm_reg_field wdog_lock;
+    function new(string name = "wdog_lock");
+        super.new(name, 32, UVM_NO_COVERAGE);
+    endfunction //new()
+
+    function void build();
+        wdog_lock = uvm_reg_field::type_id::create("wdog_lock");
+        wdog_lock.configure(this, 32, 0, "RW", 0, 32'h1ACCE551, 0, 1, 1);
+    endfunction: build
+    
+endclass //wdog_lock extends uvm_reg
